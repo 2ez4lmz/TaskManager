8836aaf6e6be04906fa959581b091ca2ac28a434
Diamond <valiullinoff@gmail.com>
2024-04-14 02:53:32 +0500

    models and migration

---
+++ TaskManager.Api/Models/Abstractions/CommonObject.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskManager.Api.Models.Abstractions
+{
+    public class CommonObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public byte[] Photo { get; set; }
+
+        public CommonObject()
+        {
+            CreationDate = DateTime.Now;
+        }
+    }
+}
---
+++ TaskManager.Api/Models/Desk.cs
@@ -0,0 +1,16 @@
+﻿using TaskManager.Api.Models.Abstractions;
+
+namespace TaskManager.Api.Models
+{
+    public class Desk : CommonObject
+    {
+        public int Id { get; set; }
+        public bool IsPrivate { get; set; }
+        public string Columns { get; set; }
+        public int AdminId { get; set; }
+        public User Admin { get; set; }
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+        public List<Task> Tasks { get; set; } = new List<Task>();
+    }
+}
